d5e0476dd46d3d78541aea301d9f5f4b3ef0f9ee
Alucard <alucardlogistics@gmail.com>
2018-07-22 16:37:25 -0500

    full CRUD features through hibernate and Spring and MySQL WorkBench

--- src/main/java/com/alucard/webCustomerTracker/dao/impl/CustomerDAOImpl.java
+++ src/main/java/com/alucard/webCustomerTracker/dao/impl/CustomerDAOImpl.java
@@ -45,9 +45,37 @@ public class CustomerDAOImpl implements CustomerDAO {
 		//get current hibernate session
 		Session currentSession = sessionFactory.getCurrentSession();
 		
-		//save the customer to the data base
-		currentSession.save(theCustomer);
-		
+		//save or update the customer to the data base
+		currentSession.saveOrUpdate(theCustomer);
 	}
+
+
+	public Customer getCustomer(int theId) {
+		
+		System.out.println("*******CustomerDAOImpl getCustomer method");
+		
+		//get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		//create a query and get customer based on the id/primary key requested
+		Customer theCustomer = currentSession.get(Customer.class, theId);
+		
+		return theCustomer;
+	}
+
+
+	public void deleteCustomer(int theId) {
+		
+		System.out.println("*******CustomerDAOImpl deleteCustomer method");
+		
+		//get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		//delete object with primary key
+		Query theQuery = currentSession.createQuery("delete from Customer where id=:customerId"); // "customerId" match with setParameter method
+		theQuery.setParameter("customerId", theId);
+		theQuery.executeUpdate();
+	}
+
 
 }
--- src/main/webapp/WEB-INF/views/listCustomers.jsp
+++ src/main/webapp/WEB-INF/views/listCustomers.jsp
@@ -32,15 +32,29 @@
 					<th>First Name</th>
 					<th>Last Name</th>
 					<th>Email</th>
+					<th>Action</th>
 				</tr>
 				
 				<!-- loop over and print our customers -->
 				<c:forEach var="tempCustomer" items="${customers}">
 				
+					<!-- construct "update" link with customer id -->
+					<c:url var="updateLink" value="/customer/showFormForUpdate">
+						<c:param name="customerId" value="${tempCustomer.id}"></c:param>
+					</c:url>
+					<!-- construct "delete" link with customer id -->
+					<c:url var="deleteLink" value="/customer/delete">
+						<c:param name="customerId" value="${tempCustomer.id}"></c:param>
+					</c:url>
 					<tr>
 						<td> ${tempCustomer.firstName} </td>
 						<td> ${tempCustomer.lastName} </td>
 						<td> ${tempCustomer.email} </td>
+						<td> 
+							<a href="${updateLink}">Update</a> | 
+							<a href="${deleteLink}">Delete</a>
+						</td>
+						
 					</tr>
 				
 				</c:forEach>
